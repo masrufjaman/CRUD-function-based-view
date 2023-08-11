d4f4a0c3e994c780e5b3904a3dfaad336270146d
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2023-08-11 19:05:14 +0600

    insert data to db

--- enroll/views.py
+++ enroll/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import Student_Registration
+from .models import User
 
 # Create your views here.
 
@@ -7,6 +8,13 @@ from .forms import Student_Registration
 def add_show(request):
     if request.method == 'POST':
         fm = Student_Registration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = Student_Registration()
     else:
         fm = Student_Registration()
-    return render(request, 'enroll/addandshow.html', {'form':fm})
+    return render(request, 'enroll/addandshow.html', {'form': fm})
