51198507476cc4b6bb94b6d7e10a61c6f86b174c
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2023-08-11 18:40:53 +0600

    forms

---
+++ enroll/forms.py
@@ -0,0 +1,14 @@
+# from django.core import validators
+from .models import User
+from django import forms
+
+
+class Student_Registration(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
--- enroll/views.py
+++ enroll/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .forms import Student_Registration
 
 # Create your views here.
 
 
 def add_show(request):
-    return render(request, 'enroll/addandshow.html')
+    if request.method == 'POST':
+        fm = Student_Registration(request.POST)
+    else:
+        fm = Student_Registration()
+    return render(request, 'enroll/addandshow.html', {'form':fm})
